44f90a9aee6d12611213a00150daaeb3a20f7971
Joseph David Santamaría Castro <85372807+JosephSantamariaCastro-TEC@users.noreply.github.com>
2022-02-23 19:54:18 -0600

    Add files via upload

---
+++ Tablero.py
@@ -0,0 +1,49 @@
+from Ficha import *
+
+class Tablero:
+    
+    #Defina aquí los atributos de Tablero
+
+    casillas = 0
+    fichas = []
+
+    #también va a necesitar una lista de Fichas (puede asumir un número de Fichas fijo si le parece más fácil), 
+    #y un mecanismo para saber quién sigue en el turno
+
+    #agregue parámetros necesarios después de self
+    #para iniciar los valores de sus atributos
+    def __init__(self , casillasp , fichasp):
+        self.casillas = casillasp
+        self.fichas = fichasp
+
+        #inicialice aquí todos los atributos
+        #no olvide usar self.atributo para acceder el atributo de la clase
+
+    #defina aquí los métodos de Tablero
+    #recuerde que el primer parámetro de cada método es siempre self
+
+    def turno(self):
+        cambiarturno=0
+        indice = 0
+
+        #realizamos un ciclo para el juego
+        while cambiarturno ==0:
+            self.fichas[indice].avanzar()
+
+
+            if (self.fichas[indice].posicion >= self.casillas ):
+                print("GANASTE" , self.fichas[indice])
+                print("SE TERMINO EL JUEGO")
+                return 
+
+            else:
+                indice = indice+1
+
+                #Si el numero de indice se sobrepasa a la cantidad de fichas se devuelve a 0 , reiniciando la ronda
+
+                if indice > len(self.fichas):
+                    indice=0
+
+
+    
+
